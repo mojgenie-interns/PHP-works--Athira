e432f0cf7ebdc7befbcb826433c43eebeaf07868
Athira-V-Anil <athiravanil05@gmail.com>
2025-06-20 17:46:00 +0530

    php works

---
+++ fn.php
@@ -0,0 +1,10 @@
+<?php
+function familyMahn($fname,$age = 45){          //defult value
+    echo "$fname as ppfamily and  age is $age" ."\n";
+    
+    
+}
+familyMahn("Arjun","90");
+familyMahn("Mariya");
+
+
